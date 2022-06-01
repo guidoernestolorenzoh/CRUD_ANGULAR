94420bb6f7b028e002556c3834877b72955abb5d
guidoernestolorenzoh <Realmadrid26*>
2022-06-01 14:38:25 -0500

    Se agrega el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,113 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {DialogComponent} from "./dialog/dialog.component";
+import {ApiService} from "./services/api.service";
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crudAngular';
+
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment', 'action'];
+  dataSource!: MatTableDataSource<any>;
+  selection!: SelectionModel<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog : MatDialog, private api : ApiService) {
+
+  }
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%'
+    }).afterClosed().subscribe(val=>{
+      if(val === 'save'){
+        this.getAllProducts();
+      }
+    })
+  }
+  getAllProducts() {
+    this.api.getProduct()
+      .subscribe({
+        next: (res) => {
+          this.dataSource = new MatTableDataSource(res);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        },
+        error: (err) => {
+          alert("Error while fetching the records");
+        }
+      })
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  editProduct(row:any){
+    this.dialog.open(DialogComponent, {
+      width:'30%',
+      data:row
+    }).afterClosed().subscribe(val=>{
+      if(val === 'updated'){
+        this.getAllProducts();
+      }
+    })
+  }
+  deleteProduct(id:number){
+    this.api.deleteProduct(id)
+      .subscribe({
+        next: (res) => {
+          alert("Product Deleted Successfully");
+          this.getAllProducts();
+        },
+        error:()=>{
+          alert("Error While deleting the product")
+        }
+      })
+  }
+
+
+  // isAllSelected() {
+  //   const numSelected = this.selection.selected.length;
+  //   const numRows = this.dataSource.data.length;
+  //   return numSelected === numRows;
+  // }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  // masterToggle() {
+  //   if (this.isAllSelected()) {
+  //     this.selection.clear();
+  //     return;
+  //   }
+  //
+  //   this.selection.select(...this.dataSource.data);
+  // }
+
+  /** The label for the checkbox on the passed row */
+  // checkboxLabel(row?: PeriodicElement): string {
+  //   if (!row) {
+  //     return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+  //   }
+  //   return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  // }
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
 }
